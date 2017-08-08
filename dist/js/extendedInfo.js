892a2193ad08278223c4d886e7fa273c7ee133ef
Silvia J Boo <sljboo@gmail.com>
2017-08-09 00:27:32 +0200

    Modified extended info to appears only when user click in basic-info

--- dist/js/extendedInfo.js
+++ dist/js/extendedInfo.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
 
-    $(".info-character-extended").on("click", function() {
-        if ($('.info-character-extended').is(':visible'))
-            $(this).hide();
-        else
-            $(this).show();
-        // $('.info-character-extended').attr("display").val()
+    $(".info-character-basics").on("click", function() {
+
+        if ($(this).hasClass('info-character-extended--active'))
+          {
+            $(this).siblings().select('.info-character-extended').removeClass('info-character-extended--active');
+           
+           } // $('.info-character-extended').removeClass('info-character-extended--active');
+        else{
+        	$('.info-character-extended').removeClass('info-character-extended--active');
+        	$(this).siblings().select('.info-character-extended').addClass('info-character-extended--active');
+            }// $('.info-character-extended').addClass('info-character-extended--active');
     });
 
 });
